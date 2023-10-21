c2ca827f35a141db92d2df6d0367c9f24ae33ed6
Your Name <[you@example.com])>
2023-10-21 12:53:51 +0530

    First commit

---
+++ Quiz/Models/score.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quiz.Models
+{
+    public class score
+    {
+        public string[] scoree = new string[20];
+    }
+}
